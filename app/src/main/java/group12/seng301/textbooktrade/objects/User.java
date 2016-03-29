6455f3b31915693b1bd92bcd2a134ffb0a7e0c4a
colinthompson <colin.thompson@ucalgary.ca>
2016-03-29 00:04:14 -0600

    Removed useless methods

--- app/src/main/java/group12/seng301/textbooktrade/objects/User.java
+++ app/src/main/java/group12/seng301/textbooktrade/objects/User.java
@@ -9,7 +9,6 @@ public class User {
 
     private String email, fullname, password;
     private RegisterActivity.Major major;
-    private ArrayList<Book> books;
 
     public User(String email, String fullname, RegisterActivity.Major major, String password) {
         this(email, fullname, major);
@@ -20,14 +19,12 @@ public class User {
         this.email = email;
         this.fullname = fullname;
         this.major = major;
-        this.password = password;
     }
 
     public User(User user) {
         this.email = user.getEmail();
         this.fullname = user.getFullname();
         this.major = user.getMajor();
-        this.books = new ArrayList<Book>(books);
     }
 
     public RegisterActivity.Major getMajor() {
@@ -50,21 +47,6 @@ public class User {
         this.major = major;
     }
 
-    public ArrayList<Book> getBooks() {
-        return new ArrayList<Book>(books);
-    }
-
-    public void addBook(Book book) {
-        books.add(new Book(book));
-    }
-
-    public void setBooks(ArrayList<Book> books) {
-        books = new ArrayList<Book>(books);
-    }
-
-    public boolean hasBooks() {
-        return !books.isEmpty();
-    }
 
 
 
