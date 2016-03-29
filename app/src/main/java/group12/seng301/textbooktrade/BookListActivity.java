503a80d7942934b9138ffc2ab86231419f272301
colinthompson <colin.thompson@ucalgary.ca>
2016-03-29 00:03:04 -0600

    Work on displaying books

--- app/src/main/java/group12/seng301/textbooktrade/BookListActivity.java
+++ app/src/main/java/group12/seng301/textbooktrade/BookListActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -35,24 +36,21 @@ public class BookListActivity extends AppCompatActivity {
      * device.
      */
     private boolean mTwoPane;
+    private static boolean upToDate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!upToDate) {
+            System.out.println("kfsklfjsalkjfk4uweoisfvjuow575653575");
+            DummyContent.setContext(this);
+            upToDate = true;
+        }
         setContentView(R.layout.activity_book_list);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Textbooks");
         setSupportActionBar(toolbar);
-        toolbar.setTitle(getTitle());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         View recyclerView = findViewById(R.id.book_list);
         assert recyclerView != null;
@@ -65,6 +63,13 @@ public class BookListActivity extends AppCompatActivity {
             // activity should be in two-pane mode.
             mTwoPane = true;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
@@ -90,8 +95,8 @@ public class BookListActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(final ViewHolder holder, int position) {
             holder.mItem = mValues.get(position);
-            holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).content);
+            holder.mTitleView.setText(mValues.get(position).book.getName());
+            holder.mAuthorView.setText(mValues.get(position).book.getAuthor());
 
             holder.mView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -108,6 +113,7 @@ public class BookListActivity extends AppCompatActivity {
                         Context context = v.getContext();
                         Intent intent = new Intent(context, BookDetailActivity.class);
                         intent.putExtra(BookDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                        intent.putExtra("UP_TO_DATE", upToDate);
 
                         context.startActivity(intent);
                     }
@@ -122,20 +128,19 @@ public class BookListActivity extends AppCompatActivity {
 
         public class ViewHolder extends RecyclerView.ViewHolder {
             public final View mView;
-            public final TextView mIdView;
-            public final TextView mContentView;
+            public final TextView mTitleView, mAuthorView;
             public DummyContent.DummyItem mItem;
 
             public ViewHolder(View view) {
                 super(view);
                 mView = view;
-                mIdView = (TextView) view.findViewById(R.id.id);
-                mContentView = (TextView) view.findViewById(R.id.content);
+                mTitleView = (TextView) view.findViewById(R.id.title);
+                mAuthorView = (TextView) view.findViewById(R.id.author);
             }
 
             @Override
             public String toString() {
-                return super.toString() + " '" + mContentView.getText() + "'";
+                return super.toString() + " '" + mTitleView.getText() + "'";
             }
         }
     }
