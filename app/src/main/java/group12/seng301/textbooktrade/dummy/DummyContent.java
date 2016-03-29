ea0e1d1abc55eed7528a3e144de284eb3bf0e2f3
colinthompson <colin.thompson@ucalgary.ca>
2016-03-29 00:05:16 -0600

    Not elegant but working content generator

--- app/src/main/java/group12/seng301/textbooktrade/dummy/DummyContent.java
+++ app/src/main/java/group12/seng301/textbooktrade/dummy/DummyContent.java
@@ -1,9 +1,14 @@
 package group12.seng301.textbooktrade.dummy;
 
+import android.content.Context;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import group12.seng301.textbooktrade.TextbooksDatabaseHelper;
+import group12.seng301.textbooktrade.objects.Book;
 
 /**
  * Helper class for providing sample content for user interfaces created by
@@ -23,13 +28,15 @@ public class DummyContent {
      */
     public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
 
-    private static final int COUNT = 25;
-
-    static {
-        // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createDummyItem(i));
+    public static void setContext(Context c) {
+        TextbooksDatabaseHelper databaseHelper = TextbooksDatabaseHelper.getInstance(c);
+        System.out.println(databaseHelper.getAllBooks().size());
+        int i = 0;
+        for (Book b : databaseHelper.getAllBooks()) {
+            addItem(createDummyItem(i, b));
+            i++;
         }
+
     }
 
     private static void addItem(DummyItem item) {
@@ -37,17 +44,8 @@ public class DummyContent {
         ITEM_MAP.put(item.id, item);
     }
 
-    private static DummyItem createDummyItem(int position) {
-        return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
-    }
-
-    private static String makeDetails(int position) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Details about Item: ").append(position);
-        for (int i = 0; i < position; i++) {
-            builder.append("\nMore details information here.");
-        }
-        return builder.toString();
+    private static DummyItem createDummyItem(int position, Book book) {
+        return new DummyItem(String.valueOf(position), book);
     }
 
     /**
@@ -55,18 +53,16 @@ public class DummyContent {
      */
     public static class DummyItem {
         public final String id;
-        public final String content;
-        public final String details;
+        public final Book book;
 
-        public DummyItem(String id, String content, String details) {
+        public DummyItem(String id, Book book) {
             this.id = id;
-            this.content = content;
-            this.details = details;
+            this.book = book;
         }
 
         @Override
         public String toString() {
-            return content;
+            return book.getName();
         }
     }
 }
